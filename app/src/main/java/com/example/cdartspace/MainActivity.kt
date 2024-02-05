1af93cb73ed9c8a0c30eb60e5038799c0eae4d67
CoolDoge <thedarkguard60@gmail.com>
2024-02-05 15:21:00 +0500

    Implement basic UI

--- app/src/main/java/com/example/cdartspace/MainActivity.kt
+++ app/src/main/java/com/example/cdartspace/MainActivity.kt
@@ -3,21 +3,24 @@ package com.example.cdartspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cdartspace.ui.theme.CDArtSpaceTheme
 
@@ -61,7 +64,22 @@ fun ArtSpace(modifier: Modifier = Modifier) {
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+            )
 
+            Text(text = "Artwork Title")
+            Text(text = "Artwork Artist")
+
+            Row {
+                Button(onClick = { /*TODO*/ }, modifier.weight(1f)) {
+                    Text(text = "Previous")
+                }
+                Button(onClick = { /*TODO*/ }, modifier.weight(1f)) {
+                    Text(text = "Next")
+                }
+            }
         }
     }
 }
