ec590554422a017242ce325a8d0b5cb23f444cd5
CoolDoge <thedarkguard60@gmail.com>
2024-02-05 17:15:40 +0500

    Implement static UI

--- app/src/main/java/com/example/cdartspace/MainActivity.kt
+++ app/src/main/java/com/example/cdartspace/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -21,7 +22,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cdartspace.ui.theme.CDArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArtSpace()
+                    ArtSpaceApp()
                 }
             }
         }
@@ -43,7 +47,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ArtSpace(modifier: Modifier = Modifier) {
+fun ArtSpaceApp(modifier: Modifier = Modifier) {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -57,22 +61,48 @@ fun ArtSpace(modifier: Modifier = Modifier) {
             )
         },
     ) { innerPadding ->
+        Surface(
+            modifier = modifier.padding(innerPadding)
+        ) {
+            ArtSpaceScreen()
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceScreen(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        // This spacer is used to push image to center
+        Spacer(modifier = modifier)
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+        )
+
+        // Bottom part of the column
         Column(
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier
-                .padding(innerPadding)
-                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null,
+            Text(
+                text = "Artwork Title",
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Artwork Artist"
             )
 
-            Text(text = "Artwork Title")
-            Text(text = "Artwork Artist")
-
-            Row {
+            Row(modifier = modifier.padding(bottom = 16.dp)) {
                 Button(onClick = { /*TODO*/ }, modifier.weight(1f)) {
                     Text(text = "Previous")
                 }
@@ -88,6 +118,6 @@ fun ArtSpace(modifier: Modifier = Modifier) {
 @Composable
 fun ArtSpacePreview() {
     CDArtSpaceTheme {
-        ArtSpace()
+        ArtSpaceApp()
     }
 }
